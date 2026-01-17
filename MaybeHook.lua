8854404cb13a257129360ec80450e6a00b3ab6c6
MaybeHookUser <IUseMaybeHook@proton.me>
2026-01-17 13:16:19 +0100

    Update MaybeHook.lua

--- MaybeHook.lua
+++ MaybeHook.lua
@@ -3,5 +3,5 @@ local UserInputService = game:GetService("UserInputService")
 if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MaybeHookUser/Mabye-Hook/refs/heads/main/Mobile"))()
 else
-    loadstring(game:HttpGet(""))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MaybeHookUser/Mabye-Hook/refs/heads/main/pc.lua"))()
 end
