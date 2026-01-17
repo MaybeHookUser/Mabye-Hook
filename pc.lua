2664222a1f5adcd69f3a20df823592b2c5694919
MaybeHookUser <IUseMaybeHook@proton.me>
2026-01-17 15:44:24 +0100

    Update pc.lua

--- pc.lua
+++ pc.lua
@@ -1,15 +1,2 @@
-if not (_G.SESSION_TOKEN and _G.Processor) or type(_G.SESSION_TOKEN) ~= "number" or type(_G.Processor) ~= "number" then
-    game.Players.LocalPlayer:Kick("Tampering (Final PC Check)")
-    return
-end
-
-_G.SESSION_TOKEN = nil
-_G.Processor = nil
-shared.SESSION_TOKEN = nil
-shared.Processor = nil
-
-print("Loading pc..")
-
-
-
-print("Loaded pc!")
+warn("Detected pc..")
+loadstring(game:HttpGet("https://pastebin.com/raw/Hm5Vmqvp"))()
